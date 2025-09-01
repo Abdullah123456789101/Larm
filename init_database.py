e295e45b0a9deff64a307192c275271b4d59690d
Ag-chr <agc@aktivit.dk>
2025-09-01 17:58:04 +0200

    ændret database layout

--- init_database.py
+++ init_database.py
@@ -11,12 +11,6 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS sensor (
                     lokale TEXT
                 )''')
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS lokale (
-                    id INTEGER PRIMARY KEY,
-                    sensor_id INTEGER,
-                    FOREIGN KEY (sensor_id) REFERENCES sensor(id)
-                )''')
-
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS data (
                     tid INTEGER,
