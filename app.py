1847c278510a2c7f03ad314306fa15e8fedbbddb
Ag-chr <agc@aktivit.dk>
2025-09-01 19:57:16 +0200

    ændret på database layout og lavet 2 endpoints

--- app.py
+++ app.py
@@ -1,6 +1,67 @@
 from flask import Flask, jsonify, request
 import sqlite3
 
-conn = sqlite3.connect('larm.db')
-cursor = conn.cursor()
 
+
+app = Flask(__name__)
+
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    conn = sqlite3.connect('larm.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM data")
+    data = cursor.fetchall()
+
+    items = []
+    for datapoint in data:
+        items.append({
+            "tid": datapoint[0],
+            "db": datapoint[1],
+            "lokale": datapoint[2],
+            "lokation": datapoint[3],
+            "sensor_id": datapoint[4]
+        })
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(items)
+
+@app.route('/data/<int:start>-<int:end>', methods=['GET'])
+def get_data_date(start, end):
+    conn = sqlite3.connect('larm.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM data WHERE tid >= ? AND tid <= ?", (start, end))
+    data = cursor.fetchall()
+
+    items = []
+    for datapoint in data:
+        items.append({
+            "tid": datapoint[0],
+            "db": datapoint[1],
+            "lokale": datapoint[2],
+            "lokation": datapoint[3],
+            "sensor_id": datapoint[4]
+        })
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(items)
+
+
+# get sensor data
+@app.route('/sensor/<int:sensor_id>/<int:start>-<int:end>', methods=['GET'])
+def get_sensor_data(sensor_id, start, end):
+    return jsonify([sensor_id, start, end])
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- init_database.py
+++ init_database.py
@@ -7,14 +7,16 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS sensor (
                     id INTEGER PRIMARY KEY,
                     navn TEXT,
-                    lokation TEXT,
-                    lokale TEXT
+                    lokale TEXT,
+                    lokation TEXT
                 )''')
 
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS data (
                     tid INTEGER,
                     db INTEGER,
+                    lokale TEXT,
+                    lokation TEXT,
                     sensor_id INTEGER,
                     FOREIGN KEY (sensor_id) REFERENCES sensor(id)
                 )''')
@@ -24,18 +26,20 @@ cursor.execute("SELECT COUNT(*) FROM sensor")
 amount = cursor.fetchone()[0]
 if amount == 0:
     print("insert sensor")
-    cursor.execute("INSERT INTO sensor (id, navn, lokation, lokale) VALUES (67, 'Zakarias', 'vindue', '2349')")
+    cursor.execute("INSERT INTO sensor (id, navn, lokale, lokation) VALUES (67, 'Zakarias', '2349', 'vindue')")
 
 cursor.execute("SELECT COUNT(*) FROM data")
 amount = cursor.fetchone()[0]
 
 if amount == 0:
     print("insert data")
-    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch(), 90, 67)")
-    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch()+10, 80, 67)")
-    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch()+20, 65, 67)")
+    cursor.execute("INSERT INTO data (tid, lokale, lokation, db, sensor_id) VALUES (unixepoch(), '2349', 'vindue', 90, 67)")
+    cursor.execute("INSERT INTO data (tid, lokale, lokation, db, sensor_id) VALUES (unixepoch()+10, '2349', 'vindue', 80, 67)")
+    cursor.execute("INSERT INTO data (tid, lokale, lokation, db, sensor_id) VALUES (unixepoch()+20, '2349', 'vindue', 65, 67)")
 
 conn.commit()
+
+
 
 cursor.close()
 conn.close()
