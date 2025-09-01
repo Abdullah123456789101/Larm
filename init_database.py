ad1c85376724f65c1c511beace40f4a7a304733c
Ag-chr <agc@aktivit.dk>
2025-09-01 19:10:57 +0200

    indsat fake data ind i database til debugging

--- init_database.py
+++ init_database.py
@@ -19,3 +19,23 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS data (
                     FOREIGN KEY (sensor_id) REFERENCES sensor(id)
                 )''')
 
+
+cursor.execute("SELECT COUNT(*) FROM sensor")
+amount = cursor.fetchone()[0]
+if amount == 0:
+    print("insert sensor")
+    cursor.execute("INSERT INTO sensor (id, navn, lokation, lokale) VALUES (67, 'Zakarias', 'vindue', '2349')")
+
+cursor.execute("SELECT COUNT(*) FROM data")
+amount = cursor.fetchone()[0]
+
+if amount == 0:
+    print("insert data")
+    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch(), 90, 67)")
+    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch()+10, 80, 67)")
+    cursor.execute("INSERT INTO data (tid, db, sensor_id) VALUES (unixepoch()+20, 65, 67)")
+
+conn.commit()
+
+cursor.close()
+conn.close()
